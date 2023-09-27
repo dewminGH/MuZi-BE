7b24a98e9ad85305103083f6ef38a4d1a41ce9d6
trashancsgo@gmail.com <trashancsgo@gmail.com>
2023-09-27 10:21:16 +0530

    fix functions names

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,7 +1,7 @@
 /*auth*/
-export { default as register } from './auth/signup';
-export { default as confirmRegister } from './auth/confirmSignUp';
-export { default as login } from './auth/signIn';
+export { default as signup } from './auth/signup';
+export { default as confirmSignUp } from './auth/confirmSignUp';
+export { default as signIn } from './auth/signIn';
 export { default as getNewTokens } from './auth/getNewTokens';
 export { default as getUser } from './auth/getUser';
 export { default as deleteUser } from './auth/deleteUser';
