b856cdb24830b1fe7950d95dde887aad6cb145a7
trashancsgo@gmail.com <trashancsgo@gmail.com>
2023-10-14 01:22:00 +0530

    nlp layer changes

--- src/functions/_nlpLayer/handler.py
+++ src/functions/_nlpLayer/handler.py
@@ -1,29 +1,22 @@
 import numpy as np
-import spacy as spaCy
+import spacy
 import json
 
-nlp = spaCy.load("en_core_web_sm")
-
-# Initialize spaCy with your specific language model
-# nlp = spacy.load("your_spacy_language_model")
+nlp = spacy.load("en_core_web_sm")
 
 
 def calculate_similarity(target, bios):
-    # Load the spaCy model (you might want to do this outside the function for efficiency)
-    # nlp = spacy.load("your_spacy_language_model")
-
-    # Calculate similarity scores for each bio
     similarity_scores = []
     for bio in bios:
-        doc = nlp(bio)
+        doc = nlp(bio["userData"])  # adjusting to new JSON structure
         similarity_score = doc.similarity(nlp(target))
         similarity_scores.append((bio, similarity_score))
 
-    # Sort the bios by similarity score in descending order
     sorted_bios = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
 
-    # Return the top 3 bios with the highest similarity scores
-    top_3_bios = [bio for bio, score in sorted_bios[:3]]
+    # Return the top 3 bios with the highest similarity scores, maintaining the user's ID
+    top_3_bios = [{"id": bio["id"], "userData": bio["userData"]}
+                  for bio, score in sorted_bios[:3]]
 
     return top_3_bios
 
@@ -31,17 +24,13 @@ def calculate_similarity(target, bios):
 
 
 def nlpLayer(event, context):
-    print("numpy", np.__version__, "spacy")
-    # Example: Using numpy and spaCy
-
+    print("numpy", np.__version__, "spacy", spacy.__version__)
     request_body = json.loads(event["body"])
-
-    # Access the "bios" attribute from the JSON data
-    bios_data = request_body.get("bios", [])
-
+    # adjusting to new JSON structure
+    user_list_data = request_body.get("userList", [])
     target_attribute = request_body.get("target")
 
-    top_3_matched_bios = calculate_similarity(target_attribute, bios_data)
+    top_3_matched_bios = calculate_similarity(target_attribute, user_list_data)
 
     print(top_3_matched_bios)
 
@@ -54,7 +43,7 @@ def nlpLayer(event, context):
         "body": json.dumps({
             "status": "running layer ....",
             "[DEBUG] running numpy_version": np.__version__,
-            "[DEBUG] running spacy": spaCy.__version__,
+            "[DEBUG] running spacy": spacy.__version__,
             "recommend profiles": top_3_matched_bios
         })
     }
